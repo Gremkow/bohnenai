6c0194f44690d8de52f16cdceb8dbb165879409b
Maurice Ackel <mackel@mail.uni-mannheim.de>
2017-09-28 15:53:16 +0200

    Changed wrong annotation

--- src/ai/BohnenspielAI.java
+++ src/ai/BohnenspielAI.java
@@ -7,7 +7,7 @@ public class BohnenspielAI {
 	Random rand = new Random();
 	
 	/**
-	* @param enemyIndex The index that refers to the field chosen by the enemy in the last action.If this value is 0, than the AI is the starting player and has to specify the first move.
+	* @param enemyIndex The index that refers to the field chosen by the enemy in the last action.If this value is -1, than the AI is the starting player and has to specify the first move.
 	* @return Return The index that refers to the field of the action chosen by this AI.
 	*/
 	public int getMove(int enemyIndex) {
