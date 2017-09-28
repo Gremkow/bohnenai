4262c5e2adddb099ffe7ea9b2c02b8c366bca654
Maurice Ackel <mackel@mail.uni-mannheim.de>
2017-09-28 16:37:40 +0200

    Added more methods that have to be filled. Please refer to the comments.

--- src/core/GameState.java
+++ src/core/GameState.java
@@ -26,19 +26,50 @@ public class GameState {
   
   // ------- FUNCTIONS --------
   
-  public void doMove(byte index) {
+  /**
+   * Distributes all seeds from index, counterclockwise
+   * @param player true = we, false = enemy
+   * @param index the index from where to take seeds
+   */
+  public void doMove(boolean player, byte index) {
     if(index >= 1 && index <= 12) {
       index--;
       for (int i = index + 1; i <= index + houses[index]; i++) {
         houses[(i % 12)]++;
       }
       houses[index] = 0;
+      //Check if someone gets beans
+      processPoint(player);
     }
     return;
   }
   
+  /**
+   * Checks if a player has scored, removes seeds from houses and adds them to players store
+   * @param player true = we, false = enemy
+   */
+  public void processPoint(boolean player) {
+    byte owned = 0;
+    
+    //TODO check if player scored somewhere, remove those seeds and add them to owned variable.
+    
+    if(player) {
+      mystore += owned;
+    } else {
+      enemystore += owned;
+    }
+  }
+  
+  /**
+   * Method to asses this state wrt the heuristic. Has to return Integer.MAX_VALUE if this is a win for us
+   * and has to return Integer.MIN_VALUE if this is a win for the enemy.
+   * 
+   * @return assesment of this state wrt heuristic
+   */
   public int assessment() {
-    return 0;
+    //simplest heuristic.. 
+    //TODO change to real heuristic
+    return mystore;
   }
   
   // -------- GETTERS ---------
