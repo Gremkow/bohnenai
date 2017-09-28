05e2e22e0a5d148c1a34aed7aaa8e3eb306f4854
Maurice Ackel <mackel@mail.uni-mannheim.de>
2017-09-28 16:38:03 +0200

    Added capability for new doMove method (players are now specified)

--- src/ai/MinMaxAI.java
+++ src/ai/MinMaxAI.java
@@ -5,6 +5,7 @@ import core.GameState;
 public class MinMaxAI {
   
   GameState state;
+  byte nextMove;
   
   public MinMaxAI() {
     this.state = new GameState((byte)6);
@@ -21,10 +22,10 @@ public class MinMaxAI {
        return 1; //return sth. between 1 and 6
      }
      //Execute enemy's move
-     state.doMove((byte)enemyIndex);
+     state.doMove(false, (byte)enemyIndex); //enemy's move
      
      //start calculating own move
-     return calculateMove(state.clone(), false, 5);
+     return calculateMove(state.clone(), 5) + offset;
    }
    
    /**
@@ -35,9 +36,54 @@ public class MinMaxAI {
     * @param depth maximum search depth
     * @return index of next move
     */
-   private byte calculateMove(GameState aiState, boolean player, int depth) {
+   private byte calculateMove(GameState startState, int depth) {
+     //start calculation
+     max(startState, depth, depth);
      
-     return 0;
+     return nextMove;
+   }
+  
+   private int max(GameState aiState, int currDepth, int desiredDepth) {
+     if(currDepth == 0) return aiState.assessment(); // last stage
+     
+     int max = Integer.MIN_VALUE; //init max value
+     
+     //Check each move
+     for(byte i = 1; i <= 6; i++) {
+       if(aiState.getSeedsInHouse(i) == 0) continue; //No move possible
+       //do move
+       GameState move = aiState.clone();
+       move.doMove(true,i); //we do the move
+       //other player
+       int value = min(move, currDepth - 1, desiredDepth);
+       if (value > max) {
+         max = value;
+         if (currDepth == desiredDepth);
+            nextMove = i;
+         }
+     }
+     return (max == Integer.MIN_VALUE) ? aiState.assessment() : max; //return max only if a move was done
+   }
+   
+   private int min(GameState aiState, int currDepth, int desiredDepth) {
+     if(currDepth == 0) return aiState.assessment(); // last stage
+     
+     int min = Integer.MAX_VALUE; //init max value
+     
+     //Check each move
+     for(byte i = 7; i <= 12; i++) {
+       if(aiState.getSeedsInHouse(i) == 0) continue; //No move possible
+       //do move
+       GameState move = aiState.clone();
+       move.doMove(false, i); //enemy does the move
+       //other player
+       int value = max(move, currDepth - 1, desiredDepth);
+       if (value < min) {
+         min = value;
+       }  
+     }
+     //No move possible (all 0)
+     return (min == Integer.MAX_VALUE) ? aiState.assessment() : min; //return min only if a move was done
    }
 
 }
